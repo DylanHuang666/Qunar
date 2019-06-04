c59b834ff90859f4bf726be2ee204de6fba1ade4
Dylan <huanglinjay123@outlook.com>
2019-06-04 22:46:42 +0800

    模拟后台数据，ajax请求

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption">
-      <swiper-slide v-for="item of swiperList" :key="item.id">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl" alt="">
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
@@ -12,24 +12,21 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
-        loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/cd67366cb7b7bda9333b9be02272f7b0.jpg_r_640x290x70_768b1307.jpg'
-      },
-      {
-        id: '0002',
-        imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/4968398bc8587f91bc21d30a44c3d2d8.jpg_r_640x290x70_e0162260.jpg'
-      },
-      {
-        id: '0003',
-        imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/6bd6c4f555472ddbad0bdaab53b0ca44.jpg_r_640x290x70_d1a74869.jpg'
-      }]
+        loop: true,
+        autoplay: 3000
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
 }
@@ -42,9 +39,8 @@ export default {
     width: 100%
     height: 0
     overflow: hidden
-    padding-bottom: 48%
+    padding-bottom: 26.666%
     background: #eee
     .swiper-img
       width: 100%
-      height: 180px
 </style>
