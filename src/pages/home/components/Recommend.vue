464239d0e46442728aff79174210d0c4917f2296
Dylan <huanglinjay123@outlook.com>
2019-06-04 13:42:58 +0800

    add recommend and weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="recommend">
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="">
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg',
+        title: '故宫',
+        desc: '世界五大宫之首，穿越与您近在咫尺'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1708/2b/2b3b94de99c0a425a3.img.jpg_200x200_2458ffb2.jpg',
+        title: '八达岭长城',
+        desc: '不到长城非好汉'
+      },
+      {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg',
+        title: '北京欢乐谷',
+        desc: '亚洲唯一飞行式过山车等你来挑战'
+      },
+      {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/36/ce3d2d6c9ab44d67ae68d940b8781829.water.jpg_200x200_0938a8f2.jpg',
+        title: '北京野生动物园',
+        desc: '敢与森林之王近距离接触吗？'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .8rem
+    font-size: .3re
+    background: #eee
+    text-indent: .2rem
+    margin-top: .3rem
+  .item-wrapper
+    display: flex
+    padding: .2rem .05rem
+    .item-img
+      width: 2rem
+      height: 2rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .65rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .55rem
+        color: #aaa
+        ellipsis()
+      .item-button
+        padding: .03rem .2rem
+        background: #ff9300
+        line-height: .44rem
+        color: #fff
+        border-radius: .08rem
+        margin-top: .15rem
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="weekend">
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
+        <div class="item-wrapper">
+          <div class="item-img-wrapper">
+            <img class="item-img" :src="item.imgUrl" alt="">
+          </div>
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        title: '京城周末撒欢',
+        desc: '在帝都过周末，不仅仅是城中游！'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/7e/476589267ebb41.jpg_r_640x214_bf599709.jpg',
+        title: '在帝都过周末，不仅仅是城中游！',
+        desc: '德智体美劳全面发展的亲子日，这些地方该去看看…'
+      },
+      {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+        title: '帝都必打卡',
+        desc: '来北京必去的景点非这些地方莫属'
+      },
+      {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg',
+        title: '寻找北京的皇城范儿',
+        desc: '数百年的宫廷庙宇，至今依旧威严霸气'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .8rem
+    font-size: .3rem
+    background: #eee
+    text-indent: .2rem
+    margin-top: .3rem
+  .item-wrapper
+    .item-img-wrapper
+      height: 0
+      overflow: hidden
+      padding-bottom: 37.06%
+      .item-img
+        width: 100%
+    .item-info
+      padding: .1rem .2rem
+      .item-title
+        line-height: .6rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .55rem
+        color: #aaa
+        ellipsis()
+</style>
