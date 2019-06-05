2d519999f9f9d2300b37d5d17584cfe0f91bf0e0
Dylan <huanglinjay123@outlook.com>
2019-06-05 13:09:31 +0800

    city-header

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    position: relative
+    height: .86rem
+    line-height: .86rem
+    color: #fff
+    text-align: center
+    background: $bgColor
+    overflow: hidden
+    font-size: .32rem
+    .header-back
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      position: absolute
+      left: 0
+      top: 0
+      color: #fff
+</style>
