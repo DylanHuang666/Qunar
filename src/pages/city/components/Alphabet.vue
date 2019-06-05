89f19638b4820cd2dac7e32885769277b1a3f33e
Dylan <huanglinjay123@outlook.com>
2019-06-05 16:26:38 +0800

    城市列表页布局完成

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="alphabet">
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">C</li>
+    <li class="item">D</li>
+    <li class="item">E</li>
+    <li class="item">F</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .alphabet
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    text-align: center
+    display: flex
+    flex-direction: column
+    justify-content: center
+    .item
+      line-height: .4rem
+      color: $bgColor
+</style>
