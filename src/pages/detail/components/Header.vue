1b7af1381c0c0f73e8ff736a8781280b452ad181
Dylan <huanglinjay123@outlook.com>
2019-06-08 19:43:59 +0800

    详情页头部下拉渐隐渐显效果

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="header">
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <div class="iconfont header-abs-back">&#xe624;</div>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>
+      故宮
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 0) {
+        this.showAbs = false
+        let opacity = top / 80
+        this.opacityStyle.opacity = opacity > 1 ? 1 : opacity
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header-abs
+    position: absolute
+    left: .2rem
+    top: .2rem
+    width: .8rem
+    height: .8rem
+    line-height: .8rem
+    border-radius: .4rem
+    background: rgba(0, 0, 0, .7)
+    .header-abs-back
+      color: #fff
+      text-align: center
+      font-size: .4rem
+  .header-fixed
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    height: .86rem
+    line-height: .86rem
+    color: #fff
+    text-align: center
+    background: $bgColor
+    overflow: hidden
+    font-size: .32rem
+    .header-fixed-back
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      position: absolute
+      left: 0
+      top: 0
+      color: #fff
+</style>
