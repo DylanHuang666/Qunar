475b9efdafca458167c8cbbb037fb7a8eaf5e585
Dylan <huanglinjay123@outlook.com>
2019-06-11 00:51:21 +0800

    build

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     proxyTable: {
       '/api': {
         target: 'https://dylanhuang666.github.io',
