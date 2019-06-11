3af7aa31ca5dd1e784dae7fc03fda89b4c408563
Dylan <huanglinjay123@outlook.com>
2019-06-11 17:44:56 +0800

    解决better-scroll插件实现滚动点击失效的问题

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -51,7 +51,7 @@ export default {
     }
   },
   mounted () {
-    this.scroll = new BScroll(this.$refs.wrapper)
+    this.scroll = new BScroll(this.$refs.wrapper, {click:true})
   },
   methods: {
     handleCityClick (city) {
